d820f306692ce577403d214cf7489f7a25a925c9
jaxxk <jackwei2018@outlook.com>
2021-10-20 18:31:39 -0700

    Demonstrating version control

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Changing the code to send a push request.',
       theme: ThemeData(
         // This is the theme of your application.
         //

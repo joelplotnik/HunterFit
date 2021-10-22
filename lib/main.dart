2189f8f0e2edd3b8c81f86dd071e99622c15715b
jaxxk <jackwei2018@outlook.com>
2021-10-22 15:51:56 -0700

    Hunter mobbing session with text to screen telling you what workouts to do

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Start doing jumping jacks NOW',
             ),
             Text(
               '$_counter',

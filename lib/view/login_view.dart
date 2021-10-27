f265a8f3059208dc8fbddec3e3e22c2f622dc513
Joel Plotnik <joelplotnik@gmail.com>
2021-10-27 11:07:16 -0700

    Clean up code in login_view.dart

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -11,92 +11,100 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFF47ABD1),
-        body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: const Color(0xFF47ABD1),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    const SizedBox(height: 150),
-                    // HunterFit logo
-                    Align(
-                      alignment: Alignment.center,
-                      child: SvgPicture.asset("assets/logo-hunter-fit.svg"),
+                const SizedBox(height: 150),
+                // HunterFit logo
+                Align(
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset("assets/logo-hunter-fit.svg"),
+                ),
+                const SizedBox(height: 30),
+                // Container for username and password
+                Container(
+                  width: 268,
+                  height: 103,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
                     ),
-                    const SizedBox(height: 30),
-                    // Container for username and password
-                    Container(
-                        width: 268,
-                        height: 103,
-                        decoration: const BoxDecoration(
-                          borderRadius : BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // Log in button
+                SizedBox(
+                  width: 268,
+                  height: 42,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          width: 268,
+                          height: 42,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                            color: Color.fromRGBO(
+                                255, 255, 255, 0.20000000298023224),
                           ),
-                          color : Color.fromRGBO(255, 255, 255, 1),
-                        )
-                    ),
-                    const SizedBox(height: 30),
-                    // Log in button
-                    SizedBox(
-                        width: 268,
-                        height: 42,
-                        child: Stack(
-                            children: <Widget>[
-                              Positioned(
-                                  top: 0,
-                                  left: 0,
-                                  child: Container(
-                                      width: 268,
-                                      height: 42,
-                                      decoration: const BoxDecoration(
-                                        borderRadius : BorderRadius.only(
-                                          topLeft: Radius.circular(20),
-                                          topRight: Radius.circular(20),
-                                          bottomLeft: Radius.circular(20),
-                                          bottomRight: Radius.circular(20),
-                                        ),
-                                        color : Color.fromRGBO(255, 255, 255, 0.20000000298023224),
-                                      )
-                                  )
-                              ),
-                              Positioned(
-                                  top: 10,
-                                  left: 109,
-                                  child: Text(loginViewModel.loginButtonText, textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                      color: Color.fromRGBO(255, 255, 255, 1),
-                                      fontFamily: 'Roboto',
-                                      fontSize: 18,
-                                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                                      fontWeight: FontWeight.normal,
-                                    ),)
-                              ),
-                            ]
-                        )
-                    ),
-                    const SizedBox(height: 30),
-                    // Sign up button
-                    Text(loginViewModel.signupButtonText, textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 1),
-                        fontFamily: 'Roboto',
-                        fontSize: 18,
-                        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                        fontWeight: FontWeight.normal,
-                        height: 1
-                    ),)
-                  ],
-                )
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        left: 109,
+                        child: Text(
+                          loginViewModel.loginButtonText,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontFamily: 'Roboto',
+                            fontSize: 18,
+                            letterSpacing:
+                                0 /*percentages not used in flutter. defaulting to zero*/,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // Sign up button
+                Text(
+                  loginViewModel.signupButtonText,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      fontFamily: 'Roboto',
+                      fontSize: 18,
+                      letterSpacing:
+                          0 /*percentages not used in flutter. defaulting to zero*/,
+                      fontWeight: FontWeight.normal,
+                      height: 1),
+                ),
               ],
             )
-        )
+          ],
+        ),
+      ),
     );
   }
 }

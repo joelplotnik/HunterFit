fe239f37f5140c0768f1338ca246b17d31451158
Alec Arce <alecvarce@gmail.com>
2021-11-07 01:45:00 -0800

    Added Weight Lifting Page

--- lib/view/weights_view.dart
+++ lib/view/weights_view.dart
@@ -14,11 +14,189 @@ class _WeightsViewState extends State<WeightsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(4, 102, 146, 1),
       appBar: AppBar(
-        title: const Text('Weights'),
+        backgroundColor: Color.fromRGBO(73, 172, 207, 1),
+        title: RichText(
+          textAlign: TextAlign.center,
+          text: const TextSpan(
+            text: "Weight Lifting",
+            style: TextStyle(fontSize: 20),
+            children: <TextSpan>[
+              TextSpan(
+                text: '\nWhat are you training today?',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Hello World i like apple'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 5,
+                ),
+                ElevatedButton(
+                  // BACK------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Back.png'),
+                    radius: 50,
+                  ),
+                ),
+                ElevatedButton(
+                  // BICEPS------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Biceps.png'),
+                    radius: 50,
+                  ),
+                ),
+                ElevatedButton(
+                  // CALVES------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Calves.png'),
+                    radius: 50,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 5,
+                ),
+                ElevatedButton(
+                  // CHEST------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Chest.png'),
+                    radius: 50,
+                  ),
+                ),
+                ElevatedButton(
+                  // CORE------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Core.png'),
+                    radius: 50,
+                  ),
+                ),
+                ElevatedButton(
+                  // GLUTES------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Glutes.png'),
+                    radius: 50,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 5,
+                ),
+                ElevatedButton(
+                  // SHOULDERS------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Shoulders.png'),
+                    radius: 50,
+                  ),
+                ),
+                ElevatedButton(
+                  // Triceps------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Triceps.png'),
+                    radius: 50,
+                  ),
+                ),
+                ElevatedButton(
+                  // Upper Legs------------------------------
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(2),
+                    onPrimary: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/Upper Legs.png'),
+                    radius: 50,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

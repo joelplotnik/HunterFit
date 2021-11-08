4269f96e8c78bec43047ff0ed7173aca8842f159
Alec Arce <alecvarce@gmail.com>
2021-11-07 17:22:02 -0800

    Improved look and feel of Weight Lifting Page

--- lib/view/weights_view.dart
+++ lib/view/weights_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hunter_fit/viewmodel/weights_viewmodel.dart';
 
 // LoginPage is the first screen a user will see upon launching the system
 class WeightsView extends StatefulWidget {
@@ -12,10 +13,12 @@ class WeightsView extends StatefulWidget {
 class _WeightsViewState extends State<WeightsView> {
   @override
   Widget build(BuildContext context) {
+    final weightsViewModel = WeightsViewModel();
+
     return Scaffold(
       backgroundColor: const Color.fromRGBO(4, 102, 146, 1),
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(73, 172, 207, 1),
+        backgroundColor: const Color.fromRGBO(73, 172, 207, 1),
         title: RichText(
           textAlign: TextAlign.center,
           text: const TextSpan(
@@ -32,170 +35,347 @@ class _WeightsViewState extends State<WeightsView> {
           ),
         ),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 60,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: Center(
+              child: Text(
+                'MAJOR MUSCLE GROUPS',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                  color: Colors.blueGrey.shade900,
+                ),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(
-                  width: 5,
-                ),
-                ElevatedButton(
-                  // BACK------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Back.png'),
-                    radius: 50,
-                  ),
-                ),
-                ElevatedButton(
-                  // BICEPS------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: CircleBorder(),
-                    padding: EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Biceps.png'),
-                    radius: 50,
-                  ),
-                ),
-                ElevatedButton(
-                  // CALVES------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Calves.png'),
-                    radius: 50,
-                  ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-              ],
+          ),
+          const SizedBox(
+            height: 20,
+            child: Divider(
+              color: Colors.white,
             ),
-            const SizedBox(
-              height: 60,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    // BACK------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Back.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Back',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    // CHEST------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Chest.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Chest',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    // SHOULDERS------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Shoulders.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Shoulders',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    // BICEPS------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Biceps.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Biceps',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    // CORE------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Core.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Core',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    // TRICEPS------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Triceps.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Triceps',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    // CALVES------------------------------
+                    onPressed: () {
+                      weightsViewModel.pressedAttention = false;
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Calves.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Calves',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    // GLUTES------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Glutes.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Glutes',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    // UPPER LEGS------------------------------
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(4),
+                      primary: Colors.blueAccent,
+                      onPrimary: Colors.black,
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage('images/Upper Legs.png'),
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Upper Legs',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Center(
+              child: Text(
+                'MINOR MUSCLE GROUPS',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                  color: Colors.blueGrey.shade900,
+                ),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(
-                  width: 5,
-                ),
-                ElevatedButton(
-                  // CHEST------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Chest.png'),
-                    radius: 50,
-                  ),
-                ),
-                ElevatedButton(
-                  // CORE------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Core.png'),
-                    radius: 50,
-                  ),
-                ),
-                ElevatedButton(
-                  // GLUTES------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Glutes.png'),
-                    radius: 50,
-                  ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-              ],
+          ),
+          const SizedBox(
+            height: 20,
+            child: Divider(
+              color: Colors.white,
             ),
-            const SizedBox(
-              height: 60,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(
-                  width: 5,
-                ),
-                ElevatedButton(
-                  // SHOULDERS------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Shoulders.png'),
-                    radius: 50,
-                  ),
-                ),
-                ElevatedButton(
-                  // Triceps------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Triceps.png'),
-                    radius: 50,
-                  ),
-                ),
-                ElevatedButton(
-                  // Upper Legs------------------------------
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(2),
-                    onPrimary: Colors.white,
-                  ),
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/Upper Legs.png'),
-                    radius: 50,
-                  ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

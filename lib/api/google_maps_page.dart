40a33508048c761633dd1ab6e23a111663b87e82
Joel Plotnik <joelplotnik@gmail.com>
2021-11-13 22:11:25 -0800

    small changes (added column) to google_maps_page.dart

--- lib/api/google_maps_page.dart
+++ lib/api/google_maps_page.dart
@@ -12,20 +12,27 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 300, // set this
-        child: Column(
-          children: [
-            Expanded(
-              child: GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: const CameraPosition(
-                    target: LatLng(33.1295, -117.1596), zoom: 18),
-                onMapCreated: (GoogleMapController controller) {},
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: const CameraPosition(
+                  target: LatLng(33.1295, -117.1596), zoom: 18),
+              onMapCreated: (GoogleMapController controller) {},
             ),
-          ],
-        ),
+          ),
+          Column(
+            children: const [
+            SizedBox(
+              height: 100,),
+              Text('Stopwatch and Start'),
+              Text('Stop, Reset buttons here'),
+              Text('Future... Save the workout'),
+              SizedBox(
+                height: 120,),
+          ],)
+        ],
       ),
     );
   }

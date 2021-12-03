d08b095beafee0db55d7d554ab2c3b08e1edda5f
Alec Arce <itssalec@gmail.com>
2021-12-03 15:22:26 -0800

    fixed weightlifting box alignment

--- lib/view/weights_view.dart
+++ lib/view/weights_view.dart
@@ -10,185 +10,111 @@ class WeightsView extends StatefulWidget {
 }
 
 class _WeightsViewState extends State<WeightsView> {
+  List<Widget> _setsList = [];
+
+  Widget _setCard() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
+      child: SizedBox(
+        height: 80,
+        width: (MediaQuery.of(context).size.width) * 0.35,
+        child: Card(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: const BorderSide(
+                color: Colors.grey,
+                width: 1,
+              )),
+          color: Colors.grey.shade300,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Expanded(
+                child: TextField(
+                  maxLength: 3,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      hintText: "0",
+                      counterText: "",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.fromLTRB(40, 25, 10, 10)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: Text(
+                  "X",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  maxLength: 3,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: "0",
+                    counterText: "",
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.fromLTRB(15, 24, 40, 10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white70,
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: const BorderSide(
-                      color: Colors.grey,
-                      width: 1,
-                    )),
-                color: Colors.grey.shade300,
-                child: Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              labelText: "Reps",
-                              contentPadding: EdgeInsets.all(40)),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(10, 40, 10, 40),
-                        child: Text(
-                          "X",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Weight",
-                            contentPadding: EdgeInsets.all(40),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: const BorderSide(
-                      color: Colors.grey,
-                      width: 1,
-                    )),
-                color: Colors.grey.shade300,
-                child: Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              labelText: "Reps",
-                              contentPadding: EdgeInsets.all(40)),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(10, 40, 10, 40),
-                        child: Text(
-                          "X",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Weight",
-                            contentPadding: EdgeInsets.all(40),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: const BorderSide(
-                      color: Colors.grey,
-                      width: 1,
-                    )),
-                color: Colors.grey.shade300,
-                child: Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              labelText: "Reps",
-                              contentPadding: EdgeInsets.all(40)),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(10, 40, 10, 40),
-                        child: Text(
-                          "X",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Weight",
-                            contentPadding: EdgeInsets.all(40),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ));
+      backgroundColor: Colors.white70,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 120,
+            width: (MediaQuery.of(context).size.width) * 0.35,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              SizedBox(width: 1),
+              Text("Sets"),
+              SizedBox(width: 30),
+              Text("Reps"),
+              SizedBox(width: 50),
+              Text("Weight"),
+              SizedBox(width: 30),
+              Text("1RM"),
+              SizedBox(width: 1),
+            ],
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _setsList.length,
+            itemBuilder: (context, index) {
+              return _setsList[index];
+            },
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.lightBlue,
+        onPressed: () {
+          setState(() {
+            _setsList.add(_setCard());
+          });
+        },
+      ),
+    );
   }
 }
-/*
-Expanded(
- child: Row(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Container(
-        margin: EdgeInsets.all(10),
-        color: Colors.blueAccent,
-        width: 3,
-        height: 50,
-      ),
-      const SizedBox(
-        width: 100,
-        child: TextField(
-          decoration: InputDecoration(labelText: "Reps"),
-        ),
-      ),
-      const SizedBox(
-        width: 10,
-      ),
-      const SizedBox(
-        width: 100,
-        child: TextField(
-          decoration: InputDecoration(labelText: "Weight"),
-        ),
-      )
-    ],
-  ),
-)
- */

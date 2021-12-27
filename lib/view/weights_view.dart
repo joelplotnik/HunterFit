f294924b86b3ca0f09322484e1b4374303eb6bcf
Alec Arce <itssalec@gmail.com>
2021-12-26 23:27:52 -0800

    Changed weights page
    s
    
    df

--- lib/view/weights_view.dart
+++ lib/view/weights_view.dart
@@ -11,6 +11,11 @@ class WeightsView extends StatefulWidget {
 
 class _WeightsViewState extends State<WeightsView> {
   List<Widget> _setsList = [];
+  int number = 1;
+
+  BoxDecoration myBoxDecoration() {
+    return BoxDecoration(border: Border.all(), color: Color(0xFF47ABD1));
+  }
 
   Widget _setCard() {
     return Padding(
@@ -69,71 +74,111 @@ class _WeightsViewState extends State<WeightsView> {
     );
   }
 
+  Widget _setCard2() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            height: 60,
+            alignment: Alignment.center,
+            child: const Text(
+              '1',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            decoration: myBoxDecoration(),
+          ),
+        ),
+        const Expanded(
+          flex: 3,
+          child: TextField(
+            textAlign: TextAlign.center,
+            maxLength: 3,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 33),
+              hintText: "Reps",
+              suffix: Text('reps'),
+              counterText: "",
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        const Expanded(
+          child: Text(
+            "X",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const Expanded(
+          flex: 3,
+          child: TextField(
+            textAlign: TextAlign.center,
+            maxLength: 3,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 31),
+              hintText: "Weight",
+              counterText: "",
+              border: InputBorder.none,
+              suffix: Text('lbs'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white70,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
+            height: 8,
+          ),
+          Container(
+            alignment: Alignment(-.9, -.1),
+            child: const Text(
+              "Barbell Bench press",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white),
+            ),
+            color: Color(0xFF47ABD1),
             height: 50,
           ),
-          Stack(
-            children: [
-              Container(
-                width: 400,
-                height: 570,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    side: const BorderSide(
-                      color: Colors.grey,
-                      width: 2,
-                    ),
-                  ),
-                  child: ListView(
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      Container(
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          image: const DecorationImage(
-                            image: AssetImage("assets/images/Deadlift.jpeg"),
-                            fit: BoxFit.fitWidth,
-                            alignment: Alignment.topCenter,
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          SizedBox(width: 2, height: 50),
-                          Text("Sets"),
-                          SizedBox(width: 30),
-                          Text("Reps"),
-                          SizedBox(width: 50),
-                          Text("Weight"),
-                          SizedBox(width: 30),
-                          Text("1RM"),
-                          SizedBox(width: 2),
-                        ],
-                      ),
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: _setsList.length,
-                        itemBuilder: (context, index) {
-                          return _setsList[index];
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          )
+          Divider(
+            color: Colors.black87,
+            height: 2,
+            thickness: 3,
+          ),
+          Expanded(
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemCount: _setsList.length,
+              itemBuilder: (context, index) {
+                return _setsList[index];
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider(
+                  color: Colors.grey,
+                  height: 1,
+                  thickness: 1,
+                );
+              },
+            ),
+          ),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
@@ -143,7 +188,7 @@ class _WeightsViewState extends State<WeightsView> {
         elevation: 7,
         onPressed: () {
           setState(() {
-            _setsList.add(_setCard());
+            _setsList.add(_setCard2());
           });
         },
       ),

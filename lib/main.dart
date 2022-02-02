2a9ca71a06fcbd7c4d1528f11aa75ff85daab361
Alec Arce <itssalec@gmail.com>
2022-02-01 17:57:55 -0800

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:hunter_fit/api/google_maps_page.dart';
 import 'package:hunter_fit/provider/location_provider.dart';
 import 'package:hunter_fit/view/login_view.dart';
 import 'package:provider/provider.dart';
-// Run HunterFit
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+// Run HunterFit
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const HunterFit());
 }
 

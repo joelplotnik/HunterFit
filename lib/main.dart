23e878f2bf222abaab14bafa84b55e7c38b5de06
Alec Arce <itssalec@gmail.com>
2022-02-01 18:06:15 -0800

    removed fb initialization in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,9 @@ import 'package:hunter_fit/api/google_maps_page.dart';
 import 'package:hunter_fit/provider/location_provider.dart';
 import 'package:hunter_fit/view/login_view.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 // Run HunterFit
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const HunterFit());
 }
 

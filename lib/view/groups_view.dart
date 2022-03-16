edca7c7d379605affc4918ffe685f294b4e1c80d
[blake046] <blake046@cougars.csusm.edu>
2022-03-15 19:31:42 -0700

    add group now adds display name instead of uid  - nicholas blake

--- lib/view/groups_view.dart
+++ lib/view/groups_view.dart
@@ -213,62 +213,7 @@ class _GroupsViewState extends State<GroupsView> {
                                                  Padding(padding: const EdgeInsets.all(20),
 
 
-                                                 child: RaisedButton(
-                                                 onPressed: () {
-                                                 showDialog(
-                                                 context: context,
-                                                 builder: (BuildContext context) {
-                                                 return AlertDialog(
-                                                 content: Stack(
-                                                 overflow: Overflow.visible,
-                                                 children: <Widget>[
-                                                 Positioned(
-                                                 right: -40.0,
-                                                 top: -40.0,
-                                                 child: InkResponse(
-                                                 onTap: () {
-                                                 Navigator.of(context).pop();
-                                                 },
-                                                 child: CircleAvatar(
-                                                 child: Icon(Icons.close),
-                                                 backgroundColor: Colors.red,
-                                                 ),
-                                                 ),
-                                                 ),
-                                                 Form(
-                                                 key: _formKey,
-                                                 child: Column(
-                                                 mainAxisSize: MainAxisSize.min,
-                                                 children: <Widget>[
-                                                 Padding(
-                                                 padding: EdgeInsets.all(8.0),
-                                                 child: TextFormField(),
-                                                 ),
-                                                 Padding(
-                                                 padding: EdgeInsets.all(8.0),
-                                                 child: TextFormField(),
-                                                 ),
-                                                 Padding(
-                                                 padding: const EdgeInsets.all(8.0),
-                                                 child: RaisedButton(
-                                                 child: Text("Submit"),
-                                                 onPressed: () {
-                                                 if (_formKey.currentState!.validate()) {
-                                                 _formKey.currentState!.save();
-                                                 }
-                                                 },
-                                                 ),
-                                                 )
-                                                 ],
-                                                 ),
-                                                 ),
-                                                 ],
-                                                 ),
-                                                 );
-                                                 });
-                                                 },
-                                                 child: Text("Invite a new Member"),
-                                                 ),
+
 
                                                  ), ], ),
                                                  );
